e57b90fd264e9d63d4e2b517385bf77ec95c9c33
miwipe <mwpedersen@snm.ku.dk>
2021-04-26 17:52:57 +0200

    Update holi_background.sh
    Updated human db

--- bin/holi_background.sh
+++ bin/holi_background.sh
@@ -64,7 +64,7 @@ echo Mapping $bname.fq against $DB
 nice -n 5 bowtie2 --threads 60 -k 5000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam 
 done
 
-for DB in /willerslev/datasets/mwp/databases/hs37d5/hs37d5
+for DB in /willerslev/edna/database/Homo_sapiens/homo_sapiens.fa
 do
 echo Mapping $bname.fq against $DB
 nice -n 5 bowtie2 --threads 60 -k 5000 -x $DB -U adap2_kmer2_$bname.pp.rmdup.fq --no-unal | samtools view -bS - > $bname.$(basename $DB).bam 
